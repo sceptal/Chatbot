ee56e61f712e1f242201cda79ddbf9c886492d7b
Max Wang <maxi.118@hotmail.com>
2022-07-04 01:08:53 +1000

    Added README, added home page url

--- chatbot_api/chatbot_api/settings.py
+++ chatbot_api/chatbot_api/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e4)p-(c)8#hjn+2n!!45^g6e#u^atxl%j8br$ik++mq83*si00'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
